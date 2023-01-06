59a49ac4d3b263b850de9ab50cad0c3e4e765fda
Marcos Paulo Ribeiro <31698863+mpr3421account@users.noreply.github.com>
2023-01-06 13:31:04 -0300

    Company Class.

---
+++ Entities/Company.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercCap10.Entities
+{
+    internal class Company : TaxPayer
+    {
+        public Company(string name, double anualIncome, int numberOfEmplyees) : base(name, anualIncome)
+        {
+            NumberOfEmployees = numberOfEmplyees;
+        }
+
+        public int NumberOfEmployees { get; set; }
+
+        public override double Tax()
+        {
+            if(NumberOfEmployees < 10)
+            {
+                return AnualIncome * 0.16;
+            }
+            else
+            {
+                return AnualIncome * 0.14;
+            }
+        }
+    }
+}
